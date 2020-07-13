efee55b6690691ee1de4137566ace3c5f4bba7a0
moaaz333 <maaz.masood144@gmail.com>
2020-07-13 12:05:18 +0500

    few lines added

--- test.r
+++ test.r
@@ -1,4 +1,2 @@
-This is another file with the print statement
-a_list = ['maaz', 'masood']
-#function to pop or remove the element on 0th index position
-a_list.pop(0)
+#This is another file with the print statement
+print('hello R')
