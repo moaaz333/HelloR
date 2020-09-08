09a1c6fd21828c920258b5090666462276f8853d
Moaaz Masood Khokhar <51307804+moaaz333@users.noreply.github.com>
2020-09-08 11:12:23 +0500

    Add files via upload

---
+++ R Programming Practice.R
@@ -0,0 +1,427 @@
+                               ### Decision Making ###
+# If Conditional Statement
+
+x <- 30L
+if(is.integer(x)) {
+  print("X is an Integer")
+}
+
+# IF Else Statement
+
+x <- c("what","is","truth")
+if("truth" %in% x) {
+  print("Truth is found")
+} else {
+  print("Truth is not found")
+}
+
+# If Else If Statements
+
+x <- c("what","is","truth")
+
+if("Truth" %in% x) {
+  print("Truth is found the first time")
+} else if ("truth" %in% x) {
+  print("truth is found the second time")
+} else {
+  print("No truth found")
+}
+
+                       ###LOOPS###
+# Repeat Loops
+v <- c("Hello","loop")
+cnt <- 2
+
+repeat {
+  print(v)
+  cnt <- cnt+1
+  
+  if(cnt > 5) {
+    break
+  }
+}
+
+# While Loops
+
+v <- c("Hello","while loop")
+cnt <- 2
+
+while (cnt < 7) {
+  print(v)
+  cnt = cnt + 1
+}
+
+# For Loop
+
+v <- LETTERS[1:4]
+for ( i in v) {
+  print(i)
+}
+                          
+
+                             ### LOOP CONTROL STATEMENTS ###
+
+# Next Statement(Contains next keyword)
+
+v <- LETTERS[1:6]
+for ( i in v) {
+  
+  if (i == "D") {
+    next
+  }
+  print(i)
+}
+
+       
+                              ### FUNCTIONS ###
+# Built in functions in R
+
+# Create a sequence of numbers from 32 to 44.
+print(seq(32,44))
+
+# Find mean of numbers from 25 to 82.
+print(mean(25:82))
+
+# Find sum of numbers frm 41 to 68.
+print(sum(41:68))
+
+# User Defined Functions
+
+# Create a function to print squares of numbers in sequence.
+new.function <- function(a) {
+  for(i in 1:a) {
+    b <- i^2
+    print(b)
+  }
+}
+
+# Create a function to print squares of numbers in sequence.
+new.function <- function(a) {
+  for(i in 1:a) {
+    b <- i^2
+    print(b)
+  }
+}
+
+# Call the function new.function supplying 6 as an argument.
+new.function(6)
+
+# Create a function without an argument.
+new.function <- function() {
+  for(i in 1:5) {
+    print(i^2)
+  }
+}	
+
+# Call the function without supplying an argument.
+new.function()
+
+# Create a function with arguments.
+new.function <- function(a,b,c) {
+  result <- a * b + c
+  print(result)
+}
+
+# Call the function by position of arguments.
+new.function(5,3,11)
+
+# Call the function by names of the arguments.
+new.function(a = 11, b = 5, c = 3)
+
+
+# LAZY EVALUATION OF FUNCTIONS
+
+# Create a function with arguments.
+new.function <- function(a, b) {
+  print(a^2)
+  print(a)
+  print(b)
+}
+
+# Evaluate the function without supplying one of the arguments.
+new.function(6)
+
+
+
+                            ### VECTOR CREATION ###
+
+# Atomic vector of type character.
+print("abc");
+
+# Atomic vector of type double.
+print(12.5)
+
+# Atomic vector of type integer.
+print(63L)
+
+# Atomic vector of type logical.
+print(TRUE)
+
+# Atomic vector of type complex.
+print(2+3i)
+
+# Atomic vector of type raw.
+print(charToRaw('hello'))
+
+
+# Multiple Elements Vector
+# Using Colon Operator with numeric data
+
+# Creating a sequence from 5 to 13.
+v <- 5:13
+print(v)
+
+# Creating a sequence from 6.6 to 12.6.
+v <- 6.6:12.6
+print(v)
+
+# If the final element specified does not belong to the sequence then it is discarded.
+v <- 3.8:11.4
+print(v)
+
+# Using Sequence(Seq.) operator
+# Create vector with elements from 5 to 9 incrementing by 0.4.
+print(seq(5, 9, by = 0.4))
+
+# Create Vector using C() function
+# The logical and numeric values are converted to characters.
+s <- c('apple','red',5,TRUE)
+print(s)
+
+# Access the vector Elements
+# Accessing vector elements using position.
+t <- c("Sun","Mon","Tue","Wed","Thurs","Fri","Sat")
+u <- t[c(2,3,6)]
+print(u)
+
+# Accessing vector elements using logical indexing.
+v <- t[c(TRUE,FALSE,FALSE,FALSE,FALSE,TRUE,FALSE)]
+print(v)
+
+# Accessing vector elements using negative indexing.
+x <- t[c(-2,-5)]
+print(x)
+
+# Accessing vector elements using 0/1 indexing.
+y <- t[c(0,0,0,0,0,0,1)]
+print(y)
+
+# VECTORS MANIPULATION
+# Create two vectors.
+v1 <- c(3,8,4,5,0,11)
+v2 <- c(4,11,0,8,1,2)
+
+# Vector addition.
+add.result <- v1+v2
+print(add.result)
+
+# Vector subtraction.
+sub.result <- v1-v2
+print(sub.result)
+
+# Vector multiplication.
+multi.result <- v1*v2
+print(multi.result)
+
+# Vector division.
+divi.result <- v1/v2
+print(divi.result)
+
+# Sorting Of Vector Elements
+
+v <- c(3,8,4,5,0,11, -9, 304)
+
+# Sort the elements of the vector.
+sort.result <- sort(v)
+print(sort.result)
+
+# Sort the elements in the reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result)
+
+# Sorting character vectors.
+v <- c("Red","Blue","yellow","violet")
+sort.result <- sort(v)
+print(sort.result)
+
+# Sorting character vectors in reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result)
+
+                             ### LISTS ###
+
+# Create a list containing strings, numbers, vectors and a logical
+# values.
+list_data <- list("Red", "Green", c(21,32,11), TRUE, 51.23, 119.1)
+print(list_data)
+
+# Naming the Elements of lists
+# Create a list containing a vector, a matrix and a list.
+list_data <- list(c("Jan","Feb","Mar"), matrix(c(3,9,5,1,-2,8), nrow = 2),list("green",12.3))
+
+# Give names to the elements in the list.
+names(list_data) <- c("1st Quarter", "A_Matrix", "A Inner list")
+
+# Show the list.
+print(list_data)
+
+# Accessing the Lists Elements
+
+# Create a list containing a vector, a matrix and a list.
+list_data <- list(c("Jan","Feb","Mar"), matrix(c(3,9,5,1,-2,8), nrow = 2),list("green",12.3))
+
+# Give names to the elements in the list.
+names(list_data) <- c("1st Quarter", "A_Matrix", "A Inner list")
+
+# Access the first element of the list.
+print(list_data[1])
+
+# Access the thrid element. As it is also a list, all its elements will be printed.
+print(list_data[3])
+
+# Access the list element using the name of the element.
+print(list_data$A_Matrix)
+
+
+# MANIPULATING LIST ELEMENTS
+
+# Create a list containing a vector, a matrix and a list.
+list_data <- list(c("Jan","Feb","Mar"), matrix(c(3,9,5,1,-2,8), nrow = 2), list("green",12.3))
+
+# Give names to the elements in the list.
+names(list_data) <- c("1st Quarter", "A_Matrix", "A Inner list")
+
+# Add element at the end of the list.
+list_data[4] <- "New element"
+print(list_data[4])
+
+# Remove the last element.
+list_data[4] <- NULL
+
+# Print the 4th Element.
+print(list_data[4])
+
+# Update the 3rd Element.
+list_data[3] <- "updated element"
+print(list_data[3])
+
+#MERGING LISTS
+# Create two lists.
+list1 <- list(1,2,3)
+list2 <- list("Sun","Mon","Tue")
+
+# Merge the two lists.
+merged.list <- c(list1,list2)
+
+# Print the merged list.
+print(merged.list)
+
+# CONVERT LIST TO VECTOR 
+# Create lists.
+list1 <- list(1:5)
+print(list1)
+
+list2 <-list(10:14)
+print(list2)
+
+# Convert the lists to vectors.
+v1 <- unlist(list1)
+v2 <- unlist(list2)
+
+print(v1)
+print(v2)
+
+# Now add the vectors
+result <- v1+v2
+print(result)
+
+                               ### MATRICS ###
+
+# Elements are arranged sequentially by row.
+M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+print(M)
+
+# Elements are arranged sequentially by column.
+N <- matrix(c(3:14), nrow = 4, byrow = FALSE)
+print(N)
+
+# Define the column and row names.
+rownames = c("row1", "row2", "row3", "row4")
+colnames = c("col1", "col2", "col3")
+
+P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
+print(P)
+
+# ACCESS THE ELEMENTS OF MATRICES
+
+# Define the column and row names.
+rownames = c("row1", "row2", "row3", "row4")
+colnames = c("col1", "col2", "col3")
+
+# Create the matrix.
+P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
+
+# Access the element at 3rd column and 1st row.
+print(P[1,3])
+
+# Access the element at 2nd column and 4th row.
+print(P[4,2])
+
+# Access only the  2nd row.
+print(P[2,])
+
+# Access only the 3rd column.
+print(P[,3])
+
+# MATRICS COMPUTATION
+
+# Create two 2x3 matrices.
+matrix1 <- matrix(c(3, 9, -1, 4, 2, 6), nrow = 2)
+print(matrix1)
+
+matrix2 <- matrix(c(5, 2, 0, 9, 3, 4), nrow = 2)
+print(matrix2)
+
+# Add the matrices.
+result <- matrix1 + matrix2
+cat("Result of addition","\n")
+print(result)
+
+# Subtract the matrices
+result <- matrix1 - matrix2
+cat("Result of subtraction","\n")
+print(result)
+
+# MATRICS MULTIPLICATION AND DIVISION
+
+# Create two 2x3 matrices.
+matrix1 <- matrix(c(3, 9, -1, 4, 2, 6), nrow = 2)
+print(matrix1)
+
+matrix2 <- matrix(c(5, 2, 0, 9, 3, 4), nrow = 2)
+print(matrix2)
+
+# Multiply the matrices.
+result <- matrix1 * matrix2
+cat("Result of multiplication","\n")
+print(result)
+
+# Divide the matrices
+result <- matrix1 / matrix2
+cat("Result of division","\n")
+print(result)
+
+                                 ### PACKAGES ###
+
+# Check Available R Packages
+# Get library locations containing R packages
+.libPaths()
+
+# Get the list of all the packages installed
+library()
+# Get all packages currently loaded in the R environment
+search()
+
+# Load the PAckage to Library
+# Load the package named "XML"
+install.packages("E:/XML_3.98-1.3.zip", repos = NULL, type = "source")
